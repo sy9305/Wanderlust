3337f3ef0c1f6edc068eb33304cc1b331ed61af0
SURAJ YADAV <surajyadav61235@gmail.com>
2025-05-26 19:06:12 +0530

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -22,7 +22,7 @@ module.exports.saveRedirectUrl = (req, res, next) =>{
 module.exports.isOwner = async (req, res, next) =>{
   let {id} = req.params;
   let listing = await Listing.findById(id);
-  if(!listing.owner.equals(res.locals.currUser._id)){
+  if(!((listing.owner) === (res.locals.currUser._id))){
     req.flash("error","You are not the owner of the listing");
     return res.redirect(`/listings/${id}`);
   }
@@ -55,7 +55,7 @@ module.exports.validateReview = (req, res, next) => {
 module.exports.isReviewAuthor = async (req, res, next) =>{
   let {id,reviewId} = req.params;
   let review = await Review.findById(reviewId);
-  if(!review.author.equals(res.locals.currUser._id)){
+  if(!((review.author) === (res.locals.currUser._id))){
     req.flash("error","You are not the author of the review");
     return res.redirect(`/listings/${id}`);
   }
